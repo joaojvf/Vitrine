3f915bbc926084a0b71a706b8fe1d430fce2216e
joaojvf <joao.victorf02@hotmail.com>
2020-05-21 18:00:36 -0300

    Add criador de criptografia de senha

---
+++ VitrineBackEnd/Vitrine.Domain/Services/CriptografiaService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Vitrine.Domain.Services
+{
+    public static class CriptografiaService
+    {
+        public static String GerarSenhaCriptografa(string value)
+        {
+            StringBuilder Sb = new StringBuilder();
+
+            using (var hash = SHA256.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                Byte[] result = hash.ComputeHash(enc.GetBytes(value));
+
+                foreach (Byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+
+            return Sb.ToString();
+        }
+    }
+}
